e60a5b8ef124348bbe2d2f729a65e11b2b015821
businesscloud <business_cloud@hotmail.com>
2013-03-16 13:15:25 +0000

    Removed println of grammars in CUP

--- final/Main.java
+++ final/Main.java
@@ -1,12 +1,3 @@
-/* 
-Commented By: Christopher Lopes 
-File Name: Main.java 
-To Create: After the scanner, lcalc.flex, and the parser, ycalc.cup, have been 
-           created. 
-                > javac Main.java 
-To Run: > java Main test.txt 
-                where test.txt is an test input file for the calculator. 
-*/
 import java.io.*;
 import java.lang.Exception;
 
@@ -22,38 +13,15 @@ public class Main {
       Object result = p.parse(); 
       if(parser.parser_successful)
       {
-    //  p.debug_parse();
-    //  if(result!=null)
 
         System.out.println("Parsing is successful");
       }
       else
       {
         System.out.println("Parsing is unsuccessful");
-
       }
-      //System.out.println(result.toString());
-    //  System.out.println("Test1");
     } catch (Exception e) { 
-      
-    //  System.out.println(e.getMessage().toString());
-      // do cleanup here -- possibly rethrow e */ 
-      //} finally { 
-        // do close out here 
         } 
-/*      String dirName = null;
-try {
 
-dirName = argv[1];
-
-parser p = new parser( new File( dirName, argv[2] ) );
-Object result = p.debug_parse(); // For debugging
-}
-catch ( Exception e ) {
-System.err.println( "Exception at " );
-e.printStackTrace();
-}*/
   } 
 }
-
-/*  Return to Main for our Calculator  */
